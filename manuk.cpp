1743fd7b51e61543745f349d210925aac2be4f34
Zireha <cheva.wahyu@gmail.com>
2023-05-06 23:24:31 +0700

    first commit

---
+++ manuk.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string input;
+
+    cout<<"masukkan input [huruf]\n";
+    getline(cin, input);
+    cout<<"mbuh lah nggateli cok semester 4";
+
+
+    return 0;
+}
